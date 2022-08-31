b7d547808d5575d75c646a7bc1e29a7f071ccb0c
Cristian Muñoz <cristianalonso.munozd@gmail.com>
2022-08-31 17:31:59 -0400

    initial

---
+++ appnews-master/src/app/services/news.service.ts
@@ -0,0 +1,39 @@
+/* eslint-disable object-shorthand */
+/* eslint-disable max-len */
+import { NewsResponse } from './../interfaces/index';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const apiKey=environment.apiKey;
+const url =environment.url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  constructor(private http: HttpClient) { }
+
+  getTopHeadlines(page: number){
+   return this.http.get<NewsResponse>(`${url}/top-headlines?`,
+   {params: {
+    apiKey: apiKey,
+    country: 'us',
+    category: 'business',
+    page: page
+   }
+  });
+  }
+
+  getTopHeadlinesByCategories(page: number, category: string){
+    return this.http.get<NewsResponse>(`${url}/top-headlines?`,
+    {params: {
+     apiKey: apiKey,
+     country: 'us',
+     category: category,
+     page: page
+    }
+   });
+   }
+}
