9e9a6b707235a8c5e26ebc4f6003b5126c79166d
Cristian Muñoz <cristianalonso.munozd@gmail.com>
2022-09-01 08:38:29 -0400

    segundo commit

--- appnews-master/capacitor.config.ts
+++ appnews-master/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'noticias',
+  appName: 'appnews-master',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- appnews-master/src/environments/environment.prod.ts
+++ appnews-master/src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  production: true,
-  apiKey: '1eb66c24b7454da6bebe27701566750d',
-  url:'https://newsapi.org/v2'
+  production: true
 };
